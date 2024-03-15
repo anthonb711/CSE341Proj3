8cab03c62b56bd8ef1a02ca9fec1174aaa570eb0
anthonb711 <anthonb711@gmail.com>
2024-03-15 18:00:36 -0400

    sessions route with stubbed GET endpoint and mongoose schema

--- models/availabilities.js
+++ models/availabilities.js
@@ -5,7 +5,7 @@ const AvailabilitySchema = new mongoose.Schema({
   userID: { type: Number, required: true },
   startTime: { type: Date, required: true },
   endTime: { type: Date, required: true },
-  isBooked: {type: String, enum: { booked: ['y', 'n']}, lowercase: true, required: true}
+  isBooked: { type: String, enum: { booked: ['y', 'n'] }, lowercase: true, required: true }
 });
 
 const Skill = mongoose.model('availabilities', AvailabilitySchema);
