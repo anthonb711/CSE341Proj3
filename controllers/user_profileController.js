20b0c1eb71ad4990409921411fbf14eb6dc14f9e
anthonb711 <anthonb711@gmail.com>
2024-03-16 21:22:52 -0400

    Adding the MongoDB connection to user_profiles GET

--- controllers/user_profileController.js
+++ controllers/user_profileController.js
@@ -2,7 +2,7 @@
 const ObjectId = require('mongodb').ObjectId;
 const UserProfile = require('../models/user_profile');
 
-const getProfile = (req, res) => {
+const getProfiles =  async(req, res) => {
   // #swagger.tags = ['User Profile']
   /* #swagger.security = [{
             "OAuth2": [
@@ -10,9 +10,14 @@ const getProfile = (req, res) => {
                 'write'
             ]
     }] */
-  res.send('Hello from user_profile');
+    try {
+    const allProfiles = await UserProfile.find();
+    res.status(200).json(allProfiles);
+  } catch (error) {
+    console.error('Error fetching user profiles:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
-
 
 // POSTS
 const addUserProfile = async (req, res) => {
@@ -41,6 +46,6 @@ const addUserProfile = async (req, res) => {
 };
 
 module.exports = {
-    getProfile,
+    getProfiles,
     addUserProfile
  };
--- routes/user_profile.js
+++ routes/user_profile.js
@@ -2,6 +2,6 @@ const routes = require('express').Router();
 const controller = require('../controllers/user_profileController');
 const { requiresAuth } = require('express-openid-connect');
 
-routes.get('/', /*requiresAuth(),*/ controller.getProfile);
+routes.get('/', /*requiresAuth(),*/ controller.getProfiles);
 
 module.exports = routes;
