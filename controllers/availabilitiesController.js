88ad438d364e03eb1fe7df02f7e0066abca3c963
anthonb711 <anthonb711@gmail.com>
2024-03-18 23:42:07 -0400

    getAvailabilities is working on localhost

--- controllers/availabilitiesController.js
+++ controllers/availabilitiesController.js
@@ -1,12 +1,26 @@
-const getAvailabilities = (req, res) => {
+const ObjectId = require('mongodb').ObjectId;
+const Availability = require('../models/availabilities');
+
+/************************************************************************
+ *  GET AVAILABILITIES- HTTP:GET
+ *************************************************************************/
+const getAvailabilities = async (req, res) => {
   // #swagger.tags = ['Availabilities']
-  /* #swagger.security = [{
-            "OAuth2": [
-                'read'
-                'write'
-            ]
-    }] */
-  res.send('Hello from availabilities');
+  //#swagger.security = [{"OAuth2": ['read', 'write']}]
+
+  try {
+    const allAvailabilities = await Availability.find();
+    res.status(200).json(allAvailabilities);
+  } catch (error) {
+    console.error('Error fetching vailabilities:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
-module.exports = { getAvailabilities };
+module.exports = {
+  getAvailabilities
+  // addAvailabilities,
+  // getAvailabilitiesId,
+  // updateAvailabilities,
+  //   deleteAvailabilities,
+};
--- routes/availabilities.js
+++ routes/availabilities.js
@@ -3,5 +3,10 @@ const controller = require('../controllers/availabilitiesController');
 const { requiresAuth } = require('express-openid-connect');
 
 routes.get('/', /*requiresAuth(),*/ controller.getAvailabilities);
+//routes.post('/', /*requiresAuth(),*/ controller.addAvailabilities);
+
+//routes.get('/:id', /*requiresAuth(),*/ controller.getAvailabilitiesId);
+//routes.put('/:id', /*requiresAuth(),*/ controller.updateAvailabilities);
+//routes.delete('/:id', /*requiresAuth(),*/ controller.deleteAvailabilities);
 
 module.exports = routes;
