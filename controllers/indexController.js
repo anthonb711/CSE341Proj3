b39338d22a7cad61654374dc3fb502a74fde05af
anthonb711 <anthonb711@gmail.com>
2024-04-03 20:47:18 -0400

    fixing the logged out condiition.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,11 @@
 const getHome = (req, res) => {
   // #swagger.tags = ['Home']
   // #swagger.produces = ['text/html']
-
-  //res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  res.send(`Heloo from Index Controller, ${JSON.stringify(req.oidc.user.name)} is ${(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')}`);
+if ((req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out') === 'Logged out') {
+    res.send(`Hello from Index Controller! User is ${(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')}`);
+} else {
+  res.send(`Hello from Index Controller! ${JSON.stringify(req.oidc.user.name)} is ${(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')}`);
+}
 };
 
 module.exports = { getHome };
